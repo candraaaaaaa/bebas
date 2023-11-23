90efa765471233bd64258c0eb5b9b4e2676cd47b
unknown <fikricandra00@gmail.com>
2023-11-22 20:02:01 -0800

    praktikum3

---
+++ src/praktikum3/PerulanganFOR.java
@@ -0,0 +1,9 @@
+package praktikum3;
+
+public class PerulanganFOR {
+	public static void main (String[] args) {
+		for(int angka=4; angka<=20; angka++) {
+			System.out.println("Angka Ke = "+ angka);
+		}
+	}
+}
---
+++ src/praktikum3/Perulanganwhile.java
@@ -0,0 +1,11 @@
+package praktikum3;
+
+public class Perulanganwhile {
+	public static void main (String[]args) {
+		int angka=21;
+		while(angka<=20) {
+			System.out.println("Angka Ke = "+angka);
+			angka++;
+		}
+	}
+}
